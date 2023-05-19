381a9b06bf0a7a9db1a5d2149229f4ff369de2e1
haliljon <juraboevhaliljon@gmail.com>
2023-05-19 19:52:56 +0500

    Create components, pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import LoginRegister from './pages/LoginRegister'
+import Reservations from './pages/Reservations'
+import AddService from './pages/AddService'
+import ReservationForm from './pages/ReservationForm'
+import Details from './pages/Details'
+import Navbar from './components/Navbar'
 
-const App = () => (
-  <h1>Welcome to HandyMate</h1>
-);
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route path='/:id' element={<Details />} >
+              <Route path='/reserve' element={<ReservationForm />} />
+            </Route>
+          </Route>
+          <Route path='/login' element={<LoginRegister />} />
+          <Route path='/reservations' element={<Reservations />} />
+          <Route path='/add-service' element={<AddService />} />
+          <Route path='/reserve_service' element={<ReservationForm />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+
+};
 
 export default App;
