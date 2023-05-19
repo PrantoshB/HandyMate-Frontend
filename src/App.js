7e8e06fa7da7d809c8c8e6f7424923292fa13b97
haliljon <juraboevhaliljon@gmail.com>
2023-05-19 19:56:41 +0500

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import LoginRegister from './pages/LoginRegister'
-import Reservations from './pages/Reservations'
-import AddService from './pages/AddService'
-import ReservationForm from './pages/ReservationForm'
-import Details from './pages/Details'
-import Navbar from './components/Navbar'
+import LoginRegister from './pages/LoginRegister';
+import Reservations from './pages/Reservations';
+import AddService from './pages/AddService';
+import ReservationForm from './pages/ReservationForm';
+import Details from './pages/Details';
+import Navbar from './components/Navbar';
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route path='/:id' element={<Details />} >
-              <Route path='/reserve' element={<ReservationForm />} />
-            </Route>
+const App = () => (
+  <>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/:id" element={<Details />}>
+            <Route path="/reserve" element={<ReservationForm />} />
           </Route>
-          <Route path='/login' element={<LoginRegister />} />
-          <Route path='/reservations' element={<Reservations />} />
-          <Route path='/add-service' element={<AddService />} />
-          <Route path='/reserve_service' element={<ReservationForm />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-
-};
+        </Route>
+        <Route path="/login" element={<LoginRegister />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/add-service" element={<AddService />} />
+        <Route path="/reserve_service" element={<ReservationForm />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
