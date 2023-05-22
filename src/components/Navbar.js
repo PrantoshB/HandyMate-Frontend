889189154185fe921ffca85e2eb0cb71112557e3
haliljon <juraboevhaliljon@gmail.com>
2023-05-22 14:51:48 +0500

    Fix linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,27 @@ import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
-    const links = [
-        { id: 1, name: 'Services', path: '/' },
-        { id: 2, name: 'Reserve', path: '/reserve' },
-        { id: 3, name: 'My Reservations', path: '/reservations' },
-        { id: 4, name: 'Add Service', path: '/add-service' },
-        { id: 5, name: 'Delete Service', path: '/' },
-    ];
-    return (
-        <header>
-            <nav>
-                <img src={logo} height={100} width={100} alt="logo" />
-                <ul>
-                    {links.map((link) => (
-                        <li key={link.id}>
-                            <NavLink to={link.path}>{link.name}</NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+  const links = [
+    { id: 1, name: 'Services', path: '/' },
+    { id: 2, name: 'Reserve', path: '/reserve' },
+    { id: 3, name: 'My Reservations', path: '/reservations' },
+    { id: 4, name: 'Add Service', path: '/add-service' },
+    { id: 5, name: 'Delete Service', path: '/' },
+  ];
+  return (
+    <header>
+      <nav>
+        <img src={logo} height={100} width={100} alt="logo" />
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path}>{link.name}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
