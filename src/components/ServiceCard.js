fa9ea1e0bde313c5e79705674fe5edab5844d3e5
Prantosh Biswas <pbiswas1007@gmail.com>
2023-05-22 20:02:10 +0530

    Add demo images

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ServiceCard = ({ name }) => (
-  <div>
+const ServiceCard = ({ name, image }) => (
+  <div style={Card}>
+    <img src={image} alt={name} />
     <h1>{name}</h1>
   </div>
 );
@@ -11,4 +12,16 @@ export default ServiceCard;
 
 ServiceCard.propTypes = {
   name: PropTypes.string.isRequired,
+  // image: PropTypes.string.isRequired,
+};
+
+const Card = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '1rem',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  transition: 'all 0.3s linear',
+  cursor: 'pointer'
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,35 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ServiceCard from '../components/ServiceCard';
 import { fetchServices } from '../store/ServicesSlice';
+import carpenter from '../assets/carpenter.png';
+import cleaning from '../assets/cleaning.png';
+import electrician from '../assets/electrician.png';
+import gardening from '../assets/gardening.png';
+import moving from '../assets/moving.png';
+import painter from '../assets/painter.png';
+import plumber from '../assets/plumber.png';
+
 
 const Home = () => {
   const dispatch = useDispatch();
   const cards = useSelector((state) => state.services.services);
+  const images = [carpenter, cleaning, electrician, gardening, moving, painter, plumber]
+
 
   useEffect(() => {
     dispatch(fetchServices());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div>
+    <div style={StyleGreed}>
       {
             cards.map((card) => (
-              <ServiceCard name={card.name} key={card.id} />
+              <ServiceCard
+              key={card.id}
+              name={card.name} 
+              image={
+                images[card.id - 1]
+              } />
             ))
         }
     </div>
@@ -23,3 +38,10 @@ const Home = () => {
 };
 
 export default Home;
+
+const StyleGreed = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+  marginTop: '1rem',
+};
