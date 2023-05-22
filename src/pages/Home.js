9cb56604fd4b88a3fbcf7aa96c35a50849d67e44
Prantosh Biswas <pbiswas1007@gmail.com>
2023-05-22 22:53:52 +0530

    Add functionality for file upload to firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,35 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ServiceCard from '../components/ServiceCard';
 import { fetchServices } from '../store/ServicesSlice';
-import carpenter from '../assets/carpenter.png';
-import cleaning from '../assets/cleaning.png';
-import electrician from '../assets/electrician.png';
-import gardening from '../assets/gardening.png';
-import moving from '../assets/moving.png';
-import painter from '../assets/painter.png';
-import plumber from '../assets/plumber.png';
-
 
 const Home = () => {
   const dispatch = useDispatch();
   const cards = useSelector((state) => state.services.services);
-  const images = [carpenter, cleaning, electrician, gardening, moving, painter, plumber]
-
 
   useEffect(() => {
     dispatch(fetchServices());
   }, [dispatch]);
 
   return (
-    <div style={StyleGreed}>j
+    <div style={StyleGreed}>
       {
             cards.map((card) => (
               <ServiceCard
               key={card.id}
               name={card.name} 
-              image={
-                images[card.id - 1]
-              } />
+              image={card.image} />
             ))
         }
     </div>
