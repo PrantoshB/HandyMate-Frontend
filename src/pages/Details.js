054e0e074f3f91a901a305c794754ba94a72202d
haliljon <juraboevhaliljon@gmail.com>
2023-05-27 12:48:55 +0500

    Fix linters

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { fetchServices } from '../store/ServicesSlice';
-import '../assets/styles/details.css'
+import '../assets/styles/details.css';
 
-const Details = ({ history }) => {
+const Details = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
@@ -20,15 +20,19 @@ const Details = ({ history }) => {
     navigate('/');
   };
   return (
-    <div className='row'>
-      <h1 id='details-heading'>{service.name}</h1>
-      <div className='col-md-8 p-5'>
-        <img src={service.image} alt={service.name} className='shadow-lg ml-md-4 img-fluid' />
-        <button type='button' className='btn btn-success rounded-pill mt-5' onClick={handleReturn}>{'<'} Return</button>
+    <div className="row">
+      <h1 id="details-heading">{service.name}</h1>
+      <div className="col-md-8 p-5">
+        <img src={service.image} alt={service.name} className="shadow-lg ml-md-4 img-fluid" />
+        <button type="button" className="btn btn-success rounded-pill mt-5" onClick={handleReturn}>
+          {'<'}
+          {' '}
+          Return
+        </button>
       </div>
-      <div className='col-md-4 '>
+      <div className="col-md-4 ">
         <h2>Details</h2>
-        <table className='table table-striped '>
+        <table className="table table-striped ">
           <tbody>
             <tr>
               <td>Description</td>
@@ -36,7 +40,10 @@ const Details = ({ history }) => {
             </tr>
             <tr>
               <td>Price</td>
-              <td>$ {service.price}</td>
+              <td>
+                $
+                {service.price}
+              </td>
             </tr>
             <tr>
               <td>Duration</td>
@@ -48,7 +55,7 @@ const Details = ({ history }) => {
             </tr>
           </tbody>
         </table>
-        <button type="button" class="btn btn-success rounded-pill">Reserve</button>
+        <button type="button" className="btn btn-success rounded-pill">Reserve</button>
       </div>
     </div>
   );
