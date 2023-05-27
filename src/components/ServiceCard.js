d1ccf0fa2ef0f70cac96a686caa72bdaec0ad784
PrantoshB <pbiswas1007@gmail.com>
2023-05-28 00:15:31 +0530

    Fix Linters Errors

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ServiceCard = ({ name, image, details, rating, price }) => (
+const ServiceCard = ({
+  name, image, details, rating, price,
+}) => (
   <div className="singlecard">
     <img src={image} alt={name} />
-    <h4 className='bold-font service-name'>{name}</h4>
-    <hr className='dash'/>
-    <div className='service-rating-price'>
-      <span className='bold-font gray-font'><i className="fa-sharp fa-solid fa-star-half-stroke"/> {rating}</span>
-      <span className='service-circle-splitter'/>
-      <span className='bold-font gray-font'><i className="fa-solid fa-dollar-sign"/> {price}</span>
+    <h4 className="bold-font service-name">{name}</h4>
+    <hr className="dash" />
+    <div className="service-rating-price">
+      <span className="bold-font gray-font">
+        <i className="fa-sharp fa-solid fa-star-half-stroke" />
+        {' '}
+        {rating}
+      </span>
+      <span className="service-circle-splitter" />
+      <span className="bold-font gray-font">
+        <i className="fa-solid fa-dollar-sign" />
+        {' '}
+        {price}
+      </span>
     </div>
-    <p className='gray-font service-details'>{details}</p>
+    <p className="gray-font service-details">{details}</p>
   </div>
 );
 
