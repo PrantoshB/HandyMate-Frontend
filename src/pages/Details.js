9394dabc25e14dab6471f9ca639836de1666ea67
Utsargo Roy <royutsargo@gmail.com>
2023-05-29 02:14:31 +0600

    Modify reservation form when it is rendered in service details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,62 +1,65 @@
-import React, { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { fetchServices } from '../store/ServicesSlice';
+import ReserveFromService from '../components/ReserveFromService';
 
 const Details = () => {
-  const navigate = useNavigate();
   const { id } = useParams();
-
   const dispatch = useDispatch();
   const services = useSelector((state) => state.services.services);
   const service = services.find((service) => service.id === Number(id));
+
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     dispatch(fetchServices());
   }, [dispatch]);
 
   const handleReserveClick = () => {
-    navigate('/reserve', { state: { serviceId: service.id.toString() } });
+    setShowForm(true);
   };
 
-  if (!service) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div>
-      <div>
-        <h1>Details</h1>
-        <img src={service.image} alt={service.name} />
-      </div>
-      <div>
-        <h2>{service.name}</h2>
-        <table>
-          <tbody>
-            <tr>
-              <td>Description</td>
-              <td>{service.details}</td>
-            </tr>
-            <tr>
-              <td>Price</td>
-              <td>{service.price}</td>
-            </tr>
-            <tr>
-              <td>Duration</td>
-              <td>
-                {service.duration}
-                {' '}
-                minutes
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <>
+      {!showForm ? (
+        <div>
+          <div>
+            <h1>Details</h1>
+            <img src={service.image} alt={service.name} />
+          </div>
+          <div>
+            <h2>{service.name}</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Description</td>
+                  <td>{service.details}</td>
+                </tr>
+                <tr>
+                  <td>Price</td>
+                  <td>{service.price}</td>
+                </tr>
+                <tr>
+                  <td>Duration</td>
+                  <td>
+                    {service.duration}
+                    {' '}
+                    minutes
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-      <button type="button" onClick={handleReserveClick} className="btn btn-warning">
-        Reserve
-      </button>
-    </div>
+          <button type="button" onClick={handleReserveClick} className="btn btn-warning">
+            Reserve
+          </button>
+        </div>
+      ) : (
+        <ReserveFromService serviceId={id} />
+      )}
+    </>
   );
 };
 
