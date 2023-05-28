43f3fa3d99432cb62ab1302ef2b970d3351b1f9f
Jacinta Ngwu <jacintangwu9900@gmail.com>
2023-05-28 11:16:40 +0000

    Modify signin.js file

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import logo from '../assets/images/hanymate-logo.png';
+import { useNavigate, Link } from 'react-router-dom';
 
 const SignIn = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error] = useState(true);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -29,32 +29,53 @@ const SignIn = () => {
   };
 
   return (
-    <section
-      className="signin-bg-image"
-    >
-      <form onSubmit={handleLogin} className="sign-form">
-        <img src={logo} alt="logo" className="logo logos" />
-        <input
-          className="sign-form__input"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="sign-form__input"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="btn"
-        >
-          Login
-        </button>
-      </form>
+    <section className="signin">
+      <div className="row">
+        <div className="side-image2">
+        </div>
+        <div className="sign__right">
+          <div className="input-box">
+            <div className="intro">
+              <span className="line" />
+              <h4 className="intro__title">Welcome to HandyMate</h4>
+              <p className="intro__text">Login to get started!</p>
+            </div>
+            <form onSubmit={handleLogin} className="sign-form">
+              <div className="input-field">
+                <input
+                  className="sign-form__input"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="input-field">
+                <input
+                  className="sign-form__input"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn"
+              >
+                Login
+              </button>
+            </form>
+            <div className='sign-in'>
+              <p>
+                Do not have an account?
+                <Link to="/signup">Sign Up</Link>
+              </p>
+              {error === false && <p>Please enter valid username and password</p>}
+        </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
