5305a893cf2a1c7127c3f7d1b87b99dacaaa5816
PrantoshB <pbiswas1007@gmail.com>
2023-05-28 12:25:46 +0530

    Refactor for mobile-first approach

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({
-  name, image, details, rating, price,
+  name, image, details, rating, price, id,
 }) => (
-  <div className="singlecard">
-    <img src={image} alt={name} />
-    <h4 className="bold-font service-name">{name}</h4>
-    <hr className="dash" />
-    <div className="service-rating-price">
-      <span className="bold-font gray-font">
-        <i className="fa-sharp fa-solid fa-star-half-stroke" />
-        {' '}
-        {rating}
-      </span>
-      <span className="service-circle-splitter" />
-      <span className="bold-font gray-font">
-        <i className="fa-solid fa-dollar-sign" />
-        {' '}
-        {price}
-      </span>
-    </div>
-    <p className="gray-font service-details">{details}</p>
+
+  <div>
+    <Link
+      to={{
+        pathname: `/services/${id}`,
+        state: { name, image, details },
+      }}
+      className="singlecard"
+    >
+      <img src={image} alt={name} />
+      <h4 className="bold-font service-name">{name}</h4>
+      <hr className="dash" />
+      <div className="service-rating-price">
+        <span className="bold-font gray-font">
+          <i className="fa-sharp fa-solid fa-star-half-stroke" />
+          {' '}
+          {rating}
+        </span>
+        <span className="service-circle-splitter" />
+        <span className="bold-font gray-font">
+          <i className="fa-solid fa-dollar-sign" />
+          {' '}
+          {price}
+        </span>
+      </div>
+      <p className="gray-font service-details">{details}</p>
+    </Link>
   </div>
 );
 
@@ -33,4 +43,5 @@ ServiceCard.propTypes = {
   details: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
   price: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
 };
