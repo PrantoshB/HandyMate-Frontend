0521d51fd02a7366fb809932a74602d73356f801
haliljon <juraboevhaliljon@gmail.com>
2023-05-29 06:17:07 +0500

    Make some amendments

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -3,17 +3,34 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const ServiceCard = ({
-  name, image, details, id,
+  name, image, details, rating, price, id,
 }) => (
-  <div className="slide">
-    <Link to={{
-      pathname: `/services/${id}`,
-      state: { name, image, details },
-    }}
+
+  <div>
+    <Link
+      to={{
+        pathname: `/services/${id}`,
+        state: { rating },
+      }}
+      className="singlecard"
     >
       <img src={image} alt={name} />
-      <h1>{name}</h1>
-      <p>{details}</p>
+      <h4 className="bold-font service-name">{name}</h4>
+      <hr className="dash" />
+      <div className="service-rating-price">
+        <span className="bold-font gray-font">
+          <i className="fa-sharp fa-solid fa-star-half-stroke" />
+          {' '}
+          {rating}
+        </span>
+        <span className="service-circle-splitter" />
+        <span className="bold-font gray-font">
+          <i className="fa-solid fa-dollar-sign" />
+          {' '}
+          {price}
+        </span>
+      </div>
+      <p className="gray-font service-details">{details}</p>
     </Link>
   </div>
 );
@@ -24,5 +41,7 @@ ServiceCard.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   details: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
   id: PropTypes.number.isRequired,
 };
