873974f8b0c27bee6b96e262a01b062023face77
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-29 12:34:52 +0300

    Fix linters

--- src/components/DeleteServiceCard.js
+++ src/components/DeleteServiceCard.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import {deleteService} from '../store/ServicesSlice';
+import { deleteService } from '../store/ServicesSlice';
 
-const DeleteServiceCard = ({ name, image, details }) =>{ 
+const DeleteServiceCard = ({ name, image, details }) => {
   const dispatch = useDispatch();
   const handleDeleteClick = (id) => {
     dispatch(deleteService(id));
-  }
+  };
   return (
-  <div className="col-md-12 delete-card d-flex p-4 align-items-center">
-    <img src={image} className="img-thumbnail rounded delete-img" />
-    <h4 className='mx-5 my-3'>{name}</h4>
-    <p className='mx-3'>{details}</p>
-    <div>
-      <button className='btn btn-danger delete-btn' onClick={handleDeleteClick}>Delete</button>
+    <div className="col-md-12 delete-card d-flex p-4 align-items-center">
+      <img src={image} alt="service" className="img-thumbnail rounded delete-img" />
+      <h4 className="mx-5 my-3">{name}</h4>
+      <p className="mx-3">{details}</p>
+      <div>
+        <button className="btn btn-danger delete-btn" type="button" onClick={handleDeleteClick}>Delete</button>
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default DeleteServiceCard;
 
